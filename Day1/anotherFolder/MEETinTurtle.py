f527a60d2ed80a580d1aca652e6505517610b130
toby20-meet <toby20@meet.mit.edu>
2018-08-15 17:13:00 +0300

    Stopping here

---
+++ Day1/anotherFolder/MEETinTurtle.py
@@ -0,0 +1,44 @@
+
+import turtle
+
+# Everything that comes after the # is a 
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+turtle.penup()
+turtle.goto(-200,-100)
+turtle.pendown()
+turtle.goto(-200,-100+200)
+turtle.goto(-200+50,-100)
+turtle.goto(-200+100,-100+200)
+turtle.goto(-200+100,-100)
+turtle.penup()
+turtle.goto(50,100)
+turtle.pendown()
+turtle.goto(50-100,100)
+turtle.goto(-50,100-200)
+turtle.goto(-50+100,-100)
+turtle.penup()
+turtle.goto(-50,0)
+turtle.pendown()
+turtle.goto(-50+100,0)
+turtle.penup()
+turtle.goto(200,100)
+turtle.pendown()
+turtle.goto(200-100,100)
+turtle.goto(100,100-200)
+turtle.goto(100+100,-100)
+turtle.penup()
+turtle.goto(100,0)
+turtle.pendown()
+turtle.goto(100+100,0)
+turtle.penup()
+turtle.goto(250,100)
+turtle.pendown()
+turtle.goto(250+100,100)
+turtle.goto(300,100)
+turtle.goto(300,100-200)
+            
+# ...and end it before the next line.
+turtle.mainloop()
---
+++ Day1/myNameinTurtle.py
@@ -0,0 +1,27 @@
+
+import turtle
+
+# Everything that comes after the # is a 
+# comment.
+# It is a note to the person reading the code.
+# The computer ignores it.
+# Write your code below here...
+turtle.goto(0,0)
+turtle.pendown()
+turtle.goto(0,0+200)
+turtle.goto(0-50,200)
+turtle.goto(-50+100,200)
+turtle.penup()
+turtle.goto(200,0)
+turtle.pendown()
+turtle.circle(100)
+turtle.penup()
+turtle.goto(350,200)
+turtle.pendown()
+turtle.goto(350,200-200)
+turtle.circle(50,180)
+turtle.right(180)
+turtle.circle(50,180)
+# ...and end it before the next line.
+turtle.mainloop()
+
